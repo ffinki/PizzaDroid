dc0846f1683dd088fc7fab274043d8767824089b
ffinki <fi40.gtr2@gmail.com>
2018-07-17 13:50:02 +0200

    Google Maps SDK setup completed

---
+++ app/src/main/java/com/droid/filip/pizzadroid/MapsActivity.java
@@ -0,0 +1,14 @@
+package com.droid.filip.pizzadroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps_activity);
+    }
+}
